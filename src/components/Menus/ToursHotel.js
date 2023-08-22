6e29a3653e7780592f25ae80253ed37321ad9052
noortechilab <juttnoor756@gmail.com>
2023-08-22 23:01:14 +0500

    init

---
+++ src/components/Menus/ToursHotel.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ToursHotel = () => {
+  return (
+    <div>ToursHotel</div>
+  )
+}
+
+export default ToursHotel
---
+++ src/routes.js
@@ -0,0 +1,78 @@
+import React from "react";
+
+import { Icon } from "@chakra-ui/react";
+import {
+  MdBarChart,
+  MdPerson,
+  MdHome,
+  MdLock,
+  MdOutlineShoppingCart,
+} from "react-icons/md";
+
+// Admin Imports
+
+import MainHome from "components/Menus/MainHome";
+import Flight from "components/Menus/Flight";
+import Bookings from "components/Menus/Bookings";
+import BusManagement from "components/Menus/BusManagement"
+import ToursHotel from "components/Menus/ToursHotel"
+import HotelManagement from "components/Menus/HotelManagement";
+// Auth Imports
+
+
+const routes = [
+  {
+    name: "Main Dashboard",
+    layout: "/admin",
+    path: "/default",
+    icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
+    component: MainHome,
+  },
+  {
+    name: "Flights Management ",
+    layout: "/admin",
+    path: "/flight-management",
+    icon: (
+      <Icon
+        as={MdOutlineShoppingCart}
+        width='20px'
+        height='20px'
+        color='inherit'
+      />
+    ),
+    component: Flight,
+    secondary: true,
+  },
+  {
+    name: "Bookings",
+    layout: "/admin",
+    icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit' />,
+    path: "/bookings",
+    component: Bookings,
+  },
+ 
+  {
+    name: "Buses Management  ",
+    layout: "/admin",
+    path: "/bus-management",
+    icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
+    component: BusManagement,
+  },
+  {
+    name: "Tours & Hotel ",
+    layout: "/admin",
+    path: "/tourshotel",
+    icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
+    component: ToursHotel,
+  },
+  {
+    name: "Hotel Management",
+    layout: "/admin",
+    path: "/Hotel-Management",
+    icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
+    component: HotelManagement,
+  },
+ 
+];
+
+export default routes;
